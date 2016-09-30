bc602e2736ae07a421cbd53cfce1e800658a9baa
acuodancer <leminhduc74@gmail.com>
2016-09-30 10:42:36 +0800

    Transfer prepareAddCommand() method from Parser to PreparedAddCommand class' own method prepare() (which overrides the parent's class prepare() method)

---
+++ src/seedu/addressbook/preparedcommands/PreparedAddCommand.java
@@ -0,0 +1,80 @@
+package seedu.addressbook.preparedcommands;
+
+import static seedu.addressbook.common.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.addressbook.commands.AddCommand;
+import seedu.addressbook.commands.Command;
+import seedu.addressbook.commands.IncorrectCommand;
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class PreparedAddCommand extends PreparedCommand {
+	
+	public static final Pattern PERSON_DATA_ARGS_FORMAT = // '/' forward slashes are reserved for delimiter prefixes
+            Pattern.compile("(?<name>[^/]+)"
+                    + " (?<isPhonePrivate>p?)p/(?<phone>[^/]+)"
+                    + " (?<isEmailPrivate>p?)e/(?<email>[^/]+)"
+                    + " (?<isAddressPrivate>p?)a/(?<address>[^/]+)"
+                    + "(?<tagArguments>(?: t/[^/]+)*)"); // variable number of tags
+
+	
+	PreparedAddCommand(String args) {
+		super(args);
+	}
+
+	@Override
+	public Command prepare(){
+        final Matcher matcher = PERSON_DATA_ARGS_FORMAT.matcher(arguments.trim());
+        // Validate arg string format
+        if (!matcher.matches()) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+        try {
+            return new AddCommand(
+                    matcher.group("name"),
+
+                    matcher.group("phone"),
+                    isPrivatePrefixPresent(matcher.group("isPhonePrivate")),
+
+                    matcher.group("email"),
+                    isPrivatePrefixPresent(matcher.group("isEmailPrivate")),
+
+                    matcher.group("address"),
+                    isPrivatePrefixPresent(matcher.group("isAddressPrivate")),
+
+                    getTagsFromArgs(matcher.group("tagArguments"))
+            );
+        } catch (IllegalValueException ive) {
+            return new IncorrectCommand(ive.getMessage());
+        }
+    }
+	
+	 /**
+     * Checks whether the private prefix of a contact detail in the add command's arguments string is present.
+     */
+    private static boolean isPrivatePrefixPresent(String matchedPrefix) {
+        return matchedPrefix.equals("p");
+    }
+    
+    /**
+     * Extracts the new person's tags from the add command's tag arguments string.
+     * Merges duplicate tag strings.
+     */
+    private static Set<String> getTagsFromArgs(String tagArguments) throws IllegalValueException {
+        // no tags
+        if (tagArguments.isEmpty()) {
+            return Collections.emptySet();
+        }
+        // replace first delimiter prefix, then split
+        final Collection<String> tagStrings = Arrays.asList(tagArguments.replaceFirst(" t/", "").split(" t/"));
+        return new HashSet<>(tagStrings);
+    }
+
+}
